7154f966bc68940887a384fe9d61fcd0bf0efb9b
Rifat-74328 <rhr74328@gmail.com>
2024-05-21 18:31:05 +0600

    dashboard design,product,sales achievement,sales target form modification,button tooltip using primeng,PascaleCase to camalCase converted

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
